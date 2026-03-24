d393e00b046683bd3c011ac0a0b2f8225a1063b3
nedarctic <nedarctic@gmail.com>
2026-03-24 09:23:11 +0300

    router refresh on evidence upload

--- components/incident/upload-evidence-form.tsx
+++ components/incident/upload-evidence-form.tsx
@@ -63,6 +63,7 @@ export default function UploadEvidenceForm({ incidentId }: { incidentId: string 
         setDialogOpen(true);
       }
     });
+    router.refresh();
   };
 
   const handleDialogClose = (open: boolean) => {
