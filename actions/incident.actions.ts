426d2a266b5f3a3458e84c5b7188d438a61c6db0
nedarctic <nedarctic@gmail.com>
2026-03-26 23:53:52 +0300

    email incident handlers on incident assign

--- actions/incident.actions.ts
+++ actions/incident.actions.ts
@@ -2,21 +2,21 @@
 
 import { prisma } from '@/lib/prisma';
 import { Incident, IncidentStatus } from '@/lib/generated/prisma/client';
-import { User } from '@/lib/generated/prisma/client';
+import { transporter } from '@/lib/mailer';
 
 export async function deleteIncident(incident: Incident) {
-    try {
-        await prisma.incident.delete({
-            where: { id: incident.id }
-        });
-        return { success: true }
-    } catch (error) {
-        return {
-            error: error instanceof
-                Error ? error.message :
-                "Unknown error"
-        }
+  try {
+    await prisma.incident.delete({
+      where: { id: incident.id }
+    });
+    return { success: true }
+  } catch (error) {
+    return {
+      error: error instanceof
+        Error ? error.message :
+        "Unknown error"
     }
+  }
 }
 
 export async function updateHandlers(
@@ -35,7 +35,23 @@ export async function updateHandlers(
         incidentId: incident.id,
         handlerId: id,
       })),
-    });
+    })
+
+    // fetch the handlers
+    const handlers = await prisma.user.findMany({
+      where: { id: { in: handlerIds } },
+      select: { email: true }
+    })
+
+    // send the handlers emails in parallel
+    Promise.all(handlers.map(({ email }) => {
+      transporter.sendMail({
+        from: `"SemaFacts" <${process.env.SMTP_USER}>`,
+        to: email,
+        subject: "Incident Assigned",
+        html: `<p>You have been assigned an incident.</p>`,
+      })
+    }))
 
     return { success: true };
   } catch (error) {
