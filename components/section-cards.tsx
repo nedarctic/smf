f932559fc6a83fd148d5c6b9fe9b1a9b119f4f7f
nedarctic <nedarctic@gmail.com>
2026-04-24 08:19:43 +0300

    force card rows on one row

--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -134,10 +134,10 @@ export function SectionCards({
   }
 
   return (
-    <div className="grid grid-cols-1 gap-4 px-4 
-    *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 
-    *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 
-    @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
+<div className="grid grid-cols-4 gap-4 px-4 
+  *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 
+  *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 
+  dark:*:data-[slot=card]:bg-card">
 
       {/* Total Incidents */}
       <Card className="@container/card">
